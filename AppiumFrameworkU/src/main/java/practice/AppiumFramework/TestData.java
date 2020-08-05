b8e0c02ff4338e836ad7068cab6cd5c01808a997
ayeshayusufgit <ayesha.yusuf@gmail.com>
2020-08-05 20:39:13 +0530

    my 4th commit

--- AppiumFrameworkU/src/main/java/practice/AppiumFramework/TestData.java
+++ AppiumFrameworkU/src/main/java/practice/AppiumFramework/TestData.java
@@ -4,6 +4,7 @@ import org.testng.annotations.DataProvider;
 
 public class TestData {
 
+	//adding comment for data provider
 	@DataProvider(name = "InputData")
 	public Object[][] getDataforEditField() {
 		// 2 sets of data, "hello" , "!@#$$"
